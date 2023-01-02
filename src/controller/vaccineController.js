08eba05539bf7d36de697bcd4a38bdf384a50ec8
newtonmahata45 <newtonmahata45@gmail.com>
2023-01-02 16:09:24 +0530

    Only1Schema

--- src/controller/vaccineController.js
+++ src/controller/vaccineController.js
@@ -13,20 +13,20 @@ const slotReg = async function (req, res) {
         if (!time[1]) { return res.status(400).send({ status: false, message: "Give `:` in between hour and minute in time formate HH:MM" }) }
         let hour = +time[0].trim()
         let minute = +time[1]
-        if (1 <= hour && hour < 5) { hour = hour + 12 }
+        if (1 <= hour && hour < 5) { hour = hour + 12 }                     //Conveting given time into Slot Time 
         if (!minute) { minute = 00 }
         if (minute > 30) {
             minute = 00;
             hour = hour + 1
         }
-        else if (1 <= minute <= 30) { minute = 30 }
+        else if (1 <= minute && minute <= 30) { minute = 30 }
         if ((!hour) || (hour > 17) || (hour < 10)) { return res.status(400).send({ status: false, message: "Hour will be in between 10-16 or 1-4 in time" }) }
-        if (minute != 0 || minute != 30) { return res.status(400).send({ status: false, message: "Minute will be in Number" }) }
+        if (minute !== 0 || minute !== 30) { return res.status(400).send({ status: false, message: "Minute will be in Number" }) }
         if (hour == 17 && minute == 00) { return res.status(400).send({ status: false, message: "16:30 is the last time slot" }) }
         date = +date
         if (!date || date < 1 || date > 30) { return res.status(400).send({ status: false, message: "Give the date in between 1-30" }) }
 
-        const slotTime = new Date(`${year}-${month}-${date} ${hour}:${minute}`);
+        const slotTime = new Date(`${year}-${month}-${date} ${hour}:${minute}`); // Conveting date and Time into Date Format 
         console.log(slotTime.getTime());
         if (slotTime == `Invalid Date`) { return res.status(400).send({ status: false, message: "Time or Date is not valid" }) }
 
@@ -75,7 +75,7 @@ const updateSlot = async function (req, res) {
         time = time.split(":")
         if (!time[1]) { return res.status(400).send({ status: false, message: "Give `:` in between hour and minute in time formate HH:MM" }) }
         let hour = +time[0].trim()
-        let minute = +time[1].trim()
+        let minute = +time[1].trim()                                //Conveting given time into Slot Time
         if (1 <= hour && hour < 5) { hour = hour + 12 }
         // if ( minute ) { return res.status(400).send({ status: false, message: "Minute will be in Number" }) }
         // if (!minute) { minute = 00 }
